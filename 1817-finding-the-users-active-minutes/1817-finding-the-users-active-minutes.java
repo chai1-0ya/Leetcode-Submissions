96f234b34dd6f4c1bcce817c5b55d833ac05b302
chai1-0ya <88584782+chai1-0ya@users.noreply.github.com>
2022-09-14 00:14:03 +0530

    Time: 1376 ms (5.24%), Space: 135.3 MB (48.38%) - LeetHub

---
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+        Set<Integer> s = new HashSet<>();
+        for(int i=0;i<logs.length;i++){
+            s.add(logs[i][0]);
+        }
+        int[] arr = new int[k];
+        Set<Integer> a = new HashSet<>();
+        Map<Integer,Integer> mp = new HashMap<>();
+        for(int x : s){
+            for(int i=0;i<logs.length;i++){
+                if(logs[i][0]==x){
+                    a.add(logs[i][1]);
+                }
+            }
+            if(mp.containsKey(a.size())){
+                int c = mp.get(a.size()) + 1;
+                mp.put(a.size(),c);
+            }
+            else 
+                mp.put(a.size(),1);
+            a.clear();
+        }
+        // int i=0;
+        for (Map.Entry<Integer,Integer> me : mp.entrySet()){
+            arr[me.getKey()-1] = me.getValue();
+        }
+        return arr;
+    }
+}
