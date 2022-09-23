4e84295bd14e90c02e7842f5d4f1b12b272ea0c9
chai1-0ya <88584782+chai1-0ya@users.noreply.github.com>
2022-09-24 02:51:08 +0530

    Time: 231 ms (63.64%), Space: 41.2 MB (66.23%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -3,7 +3,13 @@ class Solution {
         long mod = 1000000007;
         long res = 1;
         for(int i=2;i<=n;i++){
-            int c = (int)(Math.log(i)/Math.log(2)) + 1;
+            // int c = (int)(Math.log(i)/Math.log(2)) + 1;
+            int t = i;
+            int c = 0;
+            while (t != 0){
+                c++;
+                t = t>>>1;
+            }
             res = ((res<<c) % mod + i) % mod;
             // res = (res+i)%mod;
             // System.out.println(res);
