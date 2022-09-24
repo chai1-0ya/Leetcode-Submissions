828cfa85f0b37382da1d0ad6164471ff51cfd8f2
chai1-0ya <88584782+chai1-0ya@users.noreply.github.com>
2022-09-24 23:27:37 +0530

    Time: 9 ms (5.26%), Space: 103.9 MB (74.55%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -11,14 +11,22 @@
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
         ListNode ln = list1;
-        ListNode ll = new ListNode();
+        ListNode ll = list2;
+        ListNode lm = list1;
         int n = 1;
         while(list1.next != null){
-            if(n <= b)
+//             {
+//                 list1 = list1.next;
+                
+//             }
+            if(n <= b){
                 list1 = list1.next;
+                if(n<a)
+                    lm = list1;
+            }
             // System.out.println(n+" "+list1.val);
             if(n==b){
-                ll = list2;
+                // ll = list2;
                 while(list2.next != null)
                     list2 = list2.next;
                 list2.next = list1.next;
@@ -26,17 +34,18 @@ class Solution {
             }
             n++;
         }
-        list1 = ln;
-        n = 1;
-        while(list1.next != null){
-            if(n<a)
-                list1 = list1.next;
-            if(n==a){
-                list1.next = ll;
-                break;
-            }
-            n++;
-        }
+        // list1 = ln;
+        // n = 1;
+        lm.next = ll;
+//         while(list1.next != null){
+            
+//                 list1 = list1.next;
+//             if(n==a){
+//                 list1.next = ll;
+//                 break;
+//             }
+//             n++;
+//         }
         return ln;
         
     }
