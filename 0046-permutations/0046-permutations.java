ec2bdb0a0b67889fe092d788c4740ff81f0797a3
Chaitanya Krishna <88584782+chai1-0ya@users.noreply.github.com>
2022-10-24 01:05:19 +0530

    Time: 3 ms (52.23%), Space: 44.7 MB (52.77%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,16 +2,21 @@ class Solution {
     
     List<List<Integer>> ll = new ArrayList<>();
     
+    // public void prem(int[] arr, Map<Integer, Character> mp, List<Integer> l){
     public void prem(int[] arr, boolean[] visit, List<Integer> l){
         if(l.size()==arr.length){
             ll.add(new ArrayList(l));
             return;
         }
         for(int j=0;j<arr.length;j++){
+            // if(mp.get(arr[j]) == 'f'){
             if(visit[j] == false){
                 l.add(arr[j]);
+                // mp.put(arr[j], 't');
                 visit[j] = true;
+                // prem(arr, mp, l);
                 prem(arr, visit, l);
+                // mp.put(arr[j], 'f');
                 visit[j] = false;
                 l.remove(l.size()-1);
             }
@@ -24,6 +29,7 @@ class Solution {
         List<Integer> l = new ArrayList<>();
         // for(int a : nums)
         //     mp.put(a, 'f');
+        // prem(nums, mp, l);
         prem(nums, visit, l);
         return ll;
     }
