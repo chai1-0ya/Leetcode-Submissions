55deecc81b6cc3482a7625f3fca20e13183f5a9c
Chaitanya Krishna <88584782+chai1-0ya@users.noreply.github.com>
2022-10-24 01:01:55 +0530

    Time: 2 ms (80.73%), Space: 44.7 MB (52.77%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,28 +2,29 @@ class Solution {
     
     List<List<Integer>> ll = new ArrayList<>();
     
-    public void prem(int[] arr, Map<Integer,Character> mp, List<Integer> l){
+    public void prem(int[] arr, boolean[] visit, List<Integer> l){
         if(l.size()==arr.length){
             ll.add(new ArrayList(l));
             return;
         }
         for(int j=0;j<arr.length;j++){
-            if(mp.get(arr[j]) == 'f'){
+            if(visit[j] == false){
                 l.add(arr[j]);
-                mp.put(arr[j], 't');
-                prem(arr, mp, l);
-                mp.put(arr[j], 'f');
+                visit[j] = true;
+                prem(arr, visit, l);
+                visit[j] = false;
                 l.remove(l.size()-1);
             }
         }
     }
     
     public List<List<Integer>> permute(int[] nums) {
-        Map<Integer,Character> mp = new HashMap<>();
+        // Map<Integer,Character> mp = new HashMap<>();
+        boolean[] visit = new boolean[nums.length];
         List<Integer> l = new ArrayList<>();
-        for(int a : nums)
-            mp.put(a, 'f');
-        prem(nums, mp, l);
+        // for(int a : nums)
+        //     mp.put(a, 'f');
+        prem(nums, visit, l);
         return ll;
     }
 }
