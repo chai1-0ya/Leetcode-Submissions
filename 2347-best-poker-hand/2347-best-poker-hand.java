daea2ef3f752335859d5e9b3f10cb671d4fdd1e1
Chaitanya Krishna <88584782+chai1-0ya@users.noreply.github.com>
2023-01-25 05:28:35 +0530

    Time: 2 ms (25.51%), Space: 41.5 MB (35.19%) - LeetHub

--- 2347-best-poker-hand/2347-best-poker-hand.java
+++ 2347-best-poker-hand/2347-best-poker-hand.java
@@ -1,45 +1,26 @@
 class Solution {
-    public String bestHand(int[] ranks, char[] suits) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        HashMap<Character, Integer> hp = new HashMap<>();
-        for(int i=0; i<ranks.length; i++){
-            if(hm.containsKey(ranks[i])){
-                int ab= hm.get(ranks[i]);
-                int cd = ab+1;
-                hm.put(ranks[i], cd);
-         }
-         else{
-             hm.put(ranks[i],1);
-         }
-        } 
-        for(int i=0; i<suits.length; i++){
-                 if(hp.containsKey(suits[i])){
-                int ab= hp.get(suits[i]);
-                int cd = ab+1;
-                hp.put(suits[i], cd);
-         }
-         else{
-             hp.put(suits[i],1);
-         }
+    public String bestHand(int[] ranks, char[] s) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        int three = 0, two = 0;
+        for(int i=0;i<5;i++){
+            mp.put(ranks[i], mp.getOrDefault(ranks[i], 0)+1);
         }
-        ArrayList<Integer> al= new ArrayList<>();
-        for(int key : hm.keySet()){
-           al.add(hm.get(key));
-        }
-        Collections.sort(al, Collections.reverseOrder());
-        for(int i=0; i<al.size(); i++){
-            if(al.get(i)>=3){
-                return "Three of a Kind";
-            }
-            else if(al.get(i)==2){
-                return "Pair";
+        if(s[0]==s[1] && s[1]==s[2] && s[2]==s[3] && s[3]==s[4])
+            return "Flush";
+        else{
+            for(Map.Entry<Integer, Integer> val : mp.entrySet()){
+                // System.out.println(val.getValue());
+                if(val.getValue()>=3)
+                    three++;
+                else if(val.getValue()==2)
+                    two++;
             }
         }
-        for(char key:hp.keySet()){
-            if(hp.get(key)==5){
-                return "Flush";
-            }
-        }
-        return "High Card";
+        if(three > 0)
+            return "Three of a Kind";
+        else if(two > 0)
+            return "Pair";
+        else 
+            return "High Card";
     }
 }
