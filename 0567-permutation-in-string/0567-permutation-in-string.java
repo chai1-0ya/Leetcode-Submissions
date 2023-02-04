40282e3cb3032bfc2e361668c95c5d8d8e24bbed
Chaitanya Krishna <88584782+chai1-0ya@users.noreply.github.com>
2023-02-04 08:51:49 +0530

    Time: 1003 ms (11.26%), Space: 42.4 MB (65.42%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        Map<Character, Integer> mp1 = new HashMap<>();
+        Map<Character, Integer> mp2 = new HashMap<>();
+        for(int i=0;i<n;i++){
+            char c = s1.charAt(i);
+            mp1.put(c, mp1.getOrDefault(c,0)+1);
+        }
+        // System.out.println(mp1);
+        for(int i=0;i<=m-n;i++){
+            for(int j=i;j<i+n;j++){
+                char c = s2.charAt(j);
+                mp2.put(c, mp2.getOrDefault(c,0)+1);
+            }
+            // System.out.println(mp2);
+            if(mp1.equals(mp2))
+                return true;
+            mp2.clear();
+        }
+        return false;
+    }
+}
