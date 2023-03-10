be8e6a88a5860a16696a1b69f3afecee17ee0ac3
Chaitanya Krishna <88584782+chai1-0ya@users.noreply.github.com>
2023-03-10 22:50:14 +0530

    Time: 5 ms (76.11%), Space: 51.1 MB (21.05%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -1,8 +1,8 @@
 class Solution {
     public List<Integer> grayCode(int n) {
     List<Integer> ls = new LinkedList<>();
-    for (int i=0;i<1<<n;i++) 
-        ls.add(i ^ i>>1);
+    for (int i=0;i<(1<<n);i++) 
+        ls.add(i^(i>>1));
     return ls;
     }
 }
