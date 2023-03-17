63e01ce802e0312fb1403dd4735e8f3b2ae71f11
Chaitanya Krishna <88584782+chai1-0ya@users.noreply.github.com>
2023-03-18 03:46:58 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (22.28%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        int dp[]=new int[nums.length+1];
+        Arrays.fill(dp,-1);
+        return func(0, 0, nums,dp);
+    }
+    
+    public int func(int i, int sum, int[] arr,int[] dp){
+        if(i>=arr.length)
+            return sum;
+        if(dp[i]!=-1) return dp[i];
+        int a=arr[i]+func(i+2,sum,arr,dp);
+        int b=func(i+1,sum,arr,dp);
+        return dp[i] = Math.max(a,b);
+    }
+}
