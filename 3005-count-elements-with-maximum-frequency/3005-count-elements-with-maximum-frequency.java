27631434a1a39a708972633ca78d8818bb7645d4
Chaitanya Krishna <88584782+chai1-0ya@users.noreply.github.com>
2024-03-09 12:49:09 +0530

    Time: 2 ms (70.78%), Space: 42.1 MB (79.03%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,22 +1,16 @@
-public class Solution {
+class Solution {
     public int maxFrequencyElements(int[] nums) {
-        // Find the frequency of each element
-        int[] frequencies = new int[100];
-        for (int num : nums) {
-            frequencies[num - 1]++;
+        int[] fre = new int[101];
+        for(int i=0;i<nums.length;i++){
+            fre[nums[i]]++;
         }
-
-        // Determine the maximum frequency, stored in the last index of the sorted array
-        Arrays.sort(frequencies);
-        int maxFreqIndex = frequencies.length - 1;
-        int totalFrequencies = frequencies[maxFreqIndex];
-
-        // Calculate the total frequencies of elements with the maximum frequency
-        // Start from the last index and iterate right to left
-        while (maxFreqIndex > 0 && frequencies[maxFreqIndex] == frequencies[maxFreqIndex - 1]) {
-            totalFrequencies += frequencies[maxFreqIndex];
-            maxFreqIndex--;
+        int ans = 0;
+        Arrays.sort(fre);
+        int mxfre = fre[100];
+        for(int i : fre){
+            if(i==mxfre)
+                ans+=i;
         }
-        return totalFrequencies;
+        return ans;
     }
 }
