2f52c4fbc67d3d1d4e63299ad5a74728f4b5740b
Chaitanya Krishna <88584782+chai1-0ya@users.noreply.github.com>
2024-03-09 13:04:25 +0530

    Time: 55 ms (5.11%), Space: 63.5 MB (22.81%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,13 +1,11 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        int i = 0;
-        int j = 0;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]==nums2[j])
-                return nums1[i];
-            else if(nums1[i]<nums2[j])
-                i++;
-            else j++;
+        Set<Integer> t = new TreeSet<>();
+        for(int x : nums1)
+            t.add(x);
+        for(int x : nums2){
+            if(t.contains(x))
+                return x;
         }
         return -1;
     }
